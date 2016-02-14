2a0f89ea50f5c4e34cecbf7a52c45302479850f0
Matt Krugman <matt@acroname.com>
2016-02-14 08:53:01 -0700

    Created workout program class object. Moved up volume solver for exercise

--- workout.py
+++ workout.py
@@ -30,53 +30,62 @@ class ExerciseDetail(object):
 class Exercise(object):
 
 	def __init__(self, name):
-		self.name = name
-		self.sets = []
-		self.volume = 0
+		self.exercise_name = name
+		self.exercise_sets = []
+		# self.exercise_volume = 0
 
 	def add_set(self, weight, repititions = 1, time = 0.0):
-		self.sets.append(ExerciseDetail(weight, repititions, time))
+		self.exercise_sets.append(ExerciseDetail(weight, repititions, time))
 
 class Workout(object):
 
-	def __init__(self, name, dt, pmax):
-		self.name = name
-		self.dt = dt
-		self.activities = []
-		self.percentOfMax = pmax
+	def __init__(self, name, dt, pmax, volume):
+		self.workout_name = name
+		self.workout_dt = dt
+		self.workout_exercises = []
+		self.workout_percentOfMax = pmax
+		self.workout_volume = volume
 
 	def add_exercise(self, i):
-		self.activities.append(i)
+		self.workout_exercises.append(i)
 
-	def solve_activity_volume(self, activity, volume, minWeightPercent = 0.7, maxWeightPercent = 1.0, minSets = 1, maxSets = 10):
+	def add_exercise_target_volume(self, exname, maxWeight, nsets, nrepsmax = 8, minRep = 1):
+		ex = Exercise(exname)
+		for i in range(0, nsets):
+			ex.add_set(maxWeight, nrepsmax)
+		self.add_exercise(ex)
+		result = self.solve_exercise_volume(self.workout_exercises[-1], self.workout_volume)
+		self.workout_volume = result['volume']
+
+	def solve_exercise_volume(self, exercise, volume, minWeightPercent = 0.7, maxWeightPercent = 1.0, minSets = 1, maxSets = 10):
 
 		error = 1.0e9
 		xLimitMax = 100000
-		nSets = len(activity.sets)
+		nSets = len(exercise.exercise_sets)
 		ModeType = 1
+		volume_best = 0
+		maxRep = 1
+		minRep = 1
 
 		# Derive target weights for each sets
-		endWeight = activity.sets[0].weight
-		maxRep = 1
+		# while at it, find the highest rep count
+		# specified in all exercise sets.
+		endWeight = exercise.exercise_sets[-1].weight
 
 		for i in range(0, nSets):
+			exercise.exercise_sets[i].weight = round((minWeightPercent + i * ((maxWeightPercent - minWeightPercent) / (nSets - 1))) * endWeight)
+			if (exercise.exercise_sets[i].repititions > maxRep):
+				maxRep = exercise.exercise_sets[i].repititions
 
-			if (activity.sets[i].repititions > maxRep):
-				maxRep = activity.sets[i].repititions
+		if (minRep != 1):
+			for i in range(0, nSets):
+				if (exercise.exercise_sets[i].repititions < minRep):
+					minRep = exercise.exercise_sets[i].repititions
 
-		minRep = maxRep
-
-		for i in range(0, nSets):
-
-			if (activity.sets[i].repititions < minRep):
-				minRep = activity.sets[i].repititions
-
-			activity.sets[i].weight = round((minWeightPercent + i * ((maxWeightPercent - minWeightPercent) / (nSets - 1))) * endWeight)
-
-		print("- Solving activity %s for volume of %.1f kg" % (activity.name, volume))
-		# print("  Minimum reps: %d Maximum reps: %d" % (minRep, maxRep))
-		# print("  Setting %% range from %.2f to %.2f" % (minWeightPercent, maxWeightPercent))
-		# print("  Targeting %d sets for target volume" % nSets)
+		print("- Solving exercise %s for volume of %.1f kg" % (exercise.exercise_name, volume))
+		print("  Minimum reps: %d Maximum reps: %d" % (minRep, maxRep))
+		print("  Setting %% range from %.2f to %.2f" % (minWeightPercent, maxWeightPercent))
+		print("  Targeting %d sets for target volume of %.1f" % (nSets, volume))
 
 		for x in range(0, xLimitMax):
 
@@ -85,7 +94,8 @@ class Workout(object):
 
 			for i in range(0, nSets):
 				setReps_calc.append(random.randint(minRep, maxRep))
-				volume_calc = volume_calc + activity.sets[i].weight * setReps_calc[i]
+				volume_calc = volume_calc + exercise.exercise_sets[i].weight * setReps_calc[i]
+				# print("volume_calc: %f" % volume_calc)
 
 			# Get the absolute error value
 			error_total = math.fabs(volume - volume_calc)
@@ -94,6 +104,22 @@ class Workout(object):
 			error = errorResult['error']
 
 			if (errorResult['isLower']):
-				activity.volume = volume_calc
+				# print volume_calc
+				self.workout_volume = volume_calc
+				volume_best = volume_calc
+				# print volume_best
 				for i in range(0, nSets):
-					activity.sets[i].repititions = setReps_calc[i]
+					exercise.exercise_sets[i].repititions = setReps_calc[i]
+
+		return {'volume': volume_best}
+
+class WorkoutProgram(object):
+
+	def __init__(self, description, dt, volume, nweeks, username):
+		self.workoutprogram_description = description
+		self.workoutprogram_dt_start = dt
+		self.workoutprogram_volume_max = volume
+		self.workoutprogram_username = username
+		self.workoutprogram_workouts = []
+		self.workoutprogram_nWeeks = nweeks
+		self.workoutprogram_nWorkoutsPerWeek = 3
